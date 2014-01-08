a4f83a562c218fed9dbf8a1c8fce56c7fde99255
Shannon Skipper <shannonskipper@gmail.com>
2014-01-08 10:46:35 -0800

    Do not configure or compile binary Ruby.

--- share/ruby-install/ruby/functions.sh
+++ share/ruby-install/ruby/functions.sh
@@ -47,32 +47,33 @@ else
 	RUBY_VERSION_FAMILY="${RUBY_VERSION:0:3}"
 	RUBY_MIRROR="${RUBY_MIRROR:-http://cache.ruby-lang.org/pub/ruby}"
 	RUBY_URL="${RUBY_URL:-$RUBY_MIRROR/$RUBY_VERSION_FAMILY/$RUBY_ARCHIVE}"
+	
+	#
+	# Configures Ruby.
+	#
+	function configure_ruby()
+	{
+		log "Configuring ruby $RUBY_VERSION ..."
+
+		if [[ "$PACKAGE_MANAGER" == "brew" ]]; then
+			./configure --prefix="$INSTALL_DIR" \
+				    --with-opt-dir="$(brew --prefix openssl):$(brew --prefix readline):$(brew --prefix libyaml):$(brew --prefix gdbm):$(brew --prefix libffi)" \
+				    "${CONFIGURE_OPTS[@]}"
+		else
+			./configure --prefix="$INSTALL_DIR" "${CONFIGURE_OPTS[@]}"
+		fi
+	}
+
+	#
+	# Compiles Ruby.
+	#
+	function compile_ruby()
+	{
+		log "Compiling ruby $RUBY_VERSION ..."
+		make "${MAKE_OPTS[@]}"
+	}
+	
 fi
-
-#
-# Configures Ruby.
-#
-function configure_ruby()
-{
-	log "Configuring ruby $RUBY_VERSION ..."
-
-	if [[ "$PACKAGE_MANAGER" == "brew" ]]; then
-		./configure --prefix="$INSTALL_DIR" \
-			    --with-opt-dir="$(brew --prefix openssl):$(brew --prefix readline):$(brew --prefix libyaml):$(brew --prefix gdbm):$(brew --prefix libffi)" \
-			    "${CONFIGURE_OPTS[@]}"
-	else
-		./configure --prefix="$INSTALL_DIR" "${CONFIGURE_OPTS[@]}"
-	fi
-}
-
-#
-# Compiles Ruby.
-#
-function compile_ruby()
-{
-	log "Compiling ruby $RUBY_VERSION ..."
-	make "${MAKE_OPTS[@]}"
-}
 
 #
 # Installs Ruby into $INSTALL_DIR
